50fa8a20d235473bc464aa9d87cf71608938bbbc
Adriirda <106302911+Adriirda@users.noreply.github.com>
2025-09-14 19:07:14 +0800

    Initial commit

---
+++ bot.py
@@ -0,0 +1,32 @@
+
+import discord
+import os
+import requests
+import json
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def get_meme():
+  response = requests.get('https://meme-api.com/gimme')
+  json_data = json.loads(response.text)
+  return json_data['url']
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print(f'Logged on as {self.user}!')
+
+    async def on_message(self, message):
+        # Prevent the bot from replying to itself
+        if message.author == self.user:
+            return
+
+        if message.content.startswith('$meme'):
+             await message.channel.send(get_meme())
+
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = MyClient(intents=intents)
+client.run(os.getenv("DISCORD_TOKEN")) 
